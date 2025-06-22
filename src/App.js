d9686394b51a1a35a7314c6ffb96babe3ce4ccca
John <johnferlinlimoran03@gmail.com>
2025-06-22 23:49:42 +0800

    change Browser Router to Hash Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Box, Container } from "@mui/material";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
